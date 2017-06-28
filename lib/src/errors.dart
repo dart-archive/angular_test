417f179d718bf452a82cbdb1bbb9c5387749ea23
Matan Lurey <matanl@google.com>
2017-06-27 18:22:36 -0700

    Delete.

--- lib/src/errors.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-export 'package:angular_test/src/errors/generic_type_missing.dart';
-export 'package:angular_test/src/errors/test_already_running.dart';
-export 'package:angular_test/src/errors/will_never_stabilize.dart';
--- lib/src/frontend.dart
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-export 'package:angular_test/src/frontend/bed.dart'
-    show disposeAnyRunningTest, NgTestBed;
-export 'package:angular_test/src/frontend/fixture.dart' show NgTestFixture;
-export 'package:angular_test/src/frontend/stabilizer.dart'
-    show NgTestStabilizer, NgZoneStabilizer;
